44b05c0bfeca2655ec23adc59cf5a1917b8f97c6
Phat Tran <ptran@strongtie.com>
2023-12-26 07:19:26 +0700

    Add project files.

---
+++ AbilityScoreCalculator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AbilityScoreCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
